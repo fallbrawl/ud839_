3d6609abb58aa93dec04b022b2fae83e494a6a13
fallbrawl <herdemisemyrise.bt@gmail.com>
2017-02-07 23:19:03 +0200

    Added custom class

---
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -0,0 +1,19 @@
+package com.example.android.miwok;
+
+/**
+ * Created by nexus on 07.02.2017.
+ */
+public class Word {
+
+    public Word(){
+
+    }
+
+    public void getMiwokTranslation(){
+
+    }
+
+    public void getDefaultTranslation(){
+
+    }
+}
